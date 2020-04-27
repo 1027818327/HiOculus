eeebd0adcdecc5fc557cbc73e0a07489e53b8b81
邹瑞平 <156332586@qq.com>
2020-04-27 13:54:25 +0800

    碰撞变黑效果优化

---
+++ Assets/OculusManger/Scripts/OVRTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRTrigger : MonoBehaviour
+{
+    //by zrp
+    //public GameObject eyeObject;
+    OVRScreenFade ovrScreenFade;
+    //层级
+    public LayerMask layerMasks;
+    private void Awake()
+    {
+        //ovrScreenFade = eyeObject.GetComponent<OVRScreenFade>();
+        ovrScreenFade = GetComponent<OVRScreenFade>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((layerMasks.value & (int)Mathf.Pow(2, other.gameObject.layer)) == (int)Mathf.Pow(2, other.gameObject.layer))
+        {
+            ovrScreenFade.SetFadeLevel(1);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((layerMasks.value & (int)Mathf.Pow(2, other.gameObject.layer)) == (int)Mathf.Pow(2, other.gameObject.layer))
+        {
+            ovrScreenFade.SetFadeLevel(0);
+        }
+    }
+}
